018ece8132da4c3ce7ba8bf7b0d58f6691111013
LordTr1gg3r <Simonthow1996@gmail.com>
2018-10-25 12:20:24 +0100

    TodoDetail Changes

--- app/src/main/java/com/example/todo/Todo.java
+++ app/src/main/java/com/example/todo/Todo.java
@@ -1,13 +1,130 @@
 package com.example.todo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class Todo extends AppCompatActivity {
 
+    private static final int IS_SUCCESS = 0;
+    private String[] mTodos;
+    private int mTodoIndex = 0;
+
+    public static final String TAG = "TodoActivity";
+
+    // name, value pair to be returned in an intent
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+
+
+    private static final String TODO_INDEX = "com.example.todoIndex";
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
+
+        Log.d(TAG, " **** Just to say the PC is in onCreate!");
+
         setContentView(R.layout.activity_todo);
+
+
+        if (savedInstanceState != null){
+            mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
+        }
+
+        final TextView textViewTodo;
+        textViewTodo = findViewById(R.id.textViewTodo);
+
+        setTextViewComplete("");
+
+        /*
+        TODO: Refactor to data layer
+         */
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todo);
+        // display the first task from mTodo array in the textViewTodo
+        textViewTodo.setText(mTodos[mTodoIndex]);
+
+        Button buttonNext = findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                textViewTodo.setText(mTodos[mTodoIndex]);
+                setTextViewComplete("");
+            }
+        });
+
+        Button buttonTodoDetail = findViewById(R.id.buttonTodoDetail);
+        buttonTodoDetail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Note, the child class being called has a static method determining the parameter
+                // to be passed to it in the intent object
+                Intent intent = TodoDetailActivity.newIntent(Todo.this, mTodoIndex);
+
+                // second param requestCode identifies the call as there could be many "intents"
+                startActivityForResult(intent, IS_SUCCESS);
+                // The result will come back through
+                // onActivityResult(requestCode, resultCode, Intent) method
+
+            }
+        });
+
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+            if (intent != null) {
+                // data in intent from child activity
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private void updateTodoComplete(boolean is_todo_complete) {
+
+        final TextView textViewTodo;
+        textViewTodo = findViewById(R.id.textViewTodo);
+
+        if (is_todo_complete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+
+            setTextViewComplete("\u2713");
+        }
+
+    }
+
+    private void setTextViewComplete( String message ){
+        final TextView textViewComplete;
+        textViewComplete = findViewById(R.id.textViewComplete);
+
+        textViewComplete.setText(message);
+    }
+
 }
